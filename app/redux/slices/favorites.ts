05a30d658ea06ce890596e28bdcd448dc1ea7eff
RamiroFerradas <ramiferra97@gmail.com>
2023-05-31 11:09:19 -0300

    update code

--- app/redux/slices/favorites.ts
+++ app/redux/slices/favorites.ts
@@ -10,21 +10,19 @@ const initialState: Person[] = [];
 
 export const favoritesSlice = createSlice({
   name: "favorites",
-  initialState:
-    // getLocalStorage(LocalStorageTypes.FAVORITES)
-    // ? JSON.parse(getLocalStorage(LocalStorageTypes.FAVORITES) as string)
-    // :
-    initialState,
+  initialState: getLocalStorage(LocalStorageTypes.FAVORITES)
+    ? JSON.parse(getLocalStorage(LocalStorageTypes.FAVORITES) as string)
+    : initialState,
   reducers: {
     addFavorite: (state, action) => {
-      // setLocalStorage(LocalStorageTypes.FAVORITES, action.payload);
+      setLocalStorage(LocalStorageTypes.FAVORITES, action.payload);
       return action.payload;
     },
     removeFavorite: (state, action) => {
       const filteredState = current(state).filter(
         (p: Person) => p.id !== action.payload.id
       );
-      // setLocalStorage(LocalStorageTypes.FAVORITES, filteredState);
+      setLocalStorage(LocalStorageTypes.FAVORITES, filteredState);
       return filteredState;
     },
   },
